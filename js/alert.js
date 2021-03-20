09578e3c7754ed82d57b7696358df491fb8f5b3d
Simon <68190998+Octagon-simon@users.noreply.github.com>
2021-03-20 10:36:47 +0100

    Add files via upload

---
+++ js/alert.js
@@ -0,0 +1,30 @@
+//Custom Alert Dismiss Script By Octagon //
+
+/* 
+
+Still a Beginner, but going to become a Pro Soon
+
+Look me up on facebook (@firegist) and follow me for more posts!
+
+*/
+
+
+function dismiss_danger_alert() {
+    document.getElementById('custom-danger-alert').style.display="none";
+}
+
+function dismiss_success_alert() {
+    document.getElementById('custom-success-alert').style.display="none";
+}
+
+function dismiss_warning_alert() {
+    document.getElementById('custom-warning-alert').style.display="none";
+}
+
+function dismiss_info_alert() {
+    document.getElementById('custom-info-alert').style.display="none";
+}
+
+function dismiss_primary_alert() {
+    document.getElementById('custom-primary-alert').style.display="none";
+}
